e866fbc19d822a00bbb898e5b7d3010ade1a18ae
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-01 14:13:00 +0300

    Remove config annotation from application class.

--- src/main/java/tv/comnata/comnata/ComnataApplication.java
+++ src/main/java/tv/comnata/comnata/ComnataApplication.java
@@ -3,9 +3,7 @@ package tv.comnata.comnata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ComnataApplication {
 
