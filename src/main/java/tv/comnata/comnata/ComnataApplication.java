c806b7b792c2721172a27fbe58f031675b06ee17
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-02 19:27:26 +0300

    Add user service.

--- src/main/java/tv/comnata/comnata/ComnataApplication.java
+++ src/main/java/tv/comnata/comnata/ComnataApplication.java
@@ -1,10 +1,8 @@
 package tv.comnata.comnata;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+// @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ComnataApplication {
 
     public static void main(String[] args) {
