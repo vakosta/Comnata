e71ed726e8c9b6004f7a0e910f8b7f14427f228b
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-02 19:06:32 +0300

    Start rewriting to microservices.

---
+++ eureka/src/test/java/tv/comnata/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.comnata.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
