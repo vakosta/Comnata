009d89f84daef17c8156e7f2d65236ce9e299656
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-04 16:50:18 +0300

    Init auth server.

---
+++ auth-server/src/main/java/tv/comnata/authserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,7 @@
+package tv.comnata.authserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+}
