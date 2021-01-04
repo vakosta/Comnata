375210a504dd4bef225d75bfbcd60d3b5abcd451
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-04 20:01:16 +0300

    Some preparations.

--- auth-server/src/main/java/tv/comnata/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/tv/comnata/authserver/AuthServerApplication.java
@@ -2,12 +2,12 @@ package tv.comnata.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class AuthServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
     }
-
 }
--- video-service/src/main/java/tv/comnata/videoservice/VideoServiceApplication.java
+++ video-service/src/main/java/tv/comnata/videoservice/VideoServiceApplication.java
@@ -2,7 +2,9 @@ package tv.comnata.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class VideoServiceApplication {
     public static void main(String[] args) {
