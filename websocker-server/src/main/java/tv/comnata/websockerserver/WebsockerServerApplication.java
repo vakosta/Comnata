3d398750bd3c1256718e131eb02f1b9adbc8b797
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-06 19:33:04 +0300

    Add websocket-server.

---
+++ websocker-server/src/main/java/tv/comnata/websockerserver/WebsockerServerApplication.java
@@ -0,0 +1,13 @@
+package tv.comnata.websockerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsockerServerApplication.class, args);
+    }
+
+}
---
+++ websocker-server/src/test/java/tv/comnata/websockerserver/WebsockerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.comnata.websockerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
