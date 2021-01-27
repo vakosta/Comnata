cc30b134f9c9316fa4e0d97237ff99ad136adca2
Vladislav Annenkov <zzoorm@gmail.com>
2021-01-27 21:55:33 +0300

    Rename VideoController to UserController.

--- user-service/src/main/java/tv/comnata/userservice/controllers/UserController.java
+++ user-service/src/main/java/tv/comnata/userservice/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-public class VideoController {
+public class UserController {
     @GetMapping("")
     public String test1() {
         return "Ok!";
