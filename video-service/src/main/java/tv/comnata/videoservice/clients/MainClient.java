5dab3aa8ef27319ef166c32f7c5e03161730a108
Vladislav Annenkov <zzoorm@gmail.com>
2021-04-02 21:43:15 +0300

    Add Swagger.

--- video-service/src/main/java/tv/comnata/videoservice/clients/MainClient.java
+++ video-service/src/main/java/tv/comnata/videoservice/clients/MainClient.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("main-service")
 public interface MainClient {
+    @RequestMapping("/test")
+    String test();
+
     @RequestMapping("/main/createVideo")
     String createVideo(@RequestParam String videoUuid);
 
