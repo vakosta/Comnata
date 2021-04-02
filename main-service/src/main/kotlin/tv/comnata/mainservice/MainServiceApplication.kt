0648cf7f5819f3da579f7941e2308448fa5ab2c8
Vladislav Annenkov <zzoorm@gmail.com>
2021-04-02 22:29:35 +0300

    Add required dependencies.

--- main-service/src/main/kotlin/tv/comnata/mainservice/MainServiceApplication.kt
+++ main-service/src/main/kotlin/tv/comnata/mainservice/MainServiceApplication.kt
@@ -1,9 +1,19 @@
 package tv.comnata.mainservice
 
+import io.dekorate.kubernetes.annotation.ImagePullPolicy
+import io.dekorate.kubernetes.annotation.KubernetesApplication
+import io.dekorate.kubernetes.annotation.Probe
+import io.dekorate.kubernetes.annotation.ServiceType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@KubernetesApplication(
+    livenessProbe = Probe(httpActionPath = "/"),
+    readinessProbe = Probe(httpActionPath = "/"),
+    serviceType = ServiceType.NodePort,
+    imagePullPolicy = ImagePullPolicy.Always
+)
 class MainServiceApplication
 
 fun main(args: Array<String>) {
